521a7877a6127b0367326498d0cc15216724f786
Josephine Muss <jmuss07@ccs.local>
2021-09-13 09:21:47 -0400

    Simple capacitative touch code, it gives a message with which pin on a Metro Express is touched with a wire. Will/can be combined with simple servco code at a later date

---
+++ Capacitive Touch.py
@@ -0,0 +1,14 @@
+# Write your code here :-)
+import time
+import board
+import touchio
+
+touch_a4 = touchio.TouchIn(board.A4)
+touch_a5 = touchio.TouchIn(board.A5)
+
+while True:
+    if touch_a4.value:
+        print("Touched A4!!")
+
+    if touch_a5.value:
+        print("Touched A5!!")
