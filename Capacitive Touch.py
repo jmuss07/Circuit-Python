d230d7906d6a28f641380c14dda0f6c700fdcb4f
Josephine Muss <jmuss07@ccs.local>
2021-09-14 13:32:27 -0400

    Added a rest at the end of each 'if' loop

--- Capacitive Touch.py
+++ Capacitive Touch.py
@@ -9,6 +9,8 @@ touch_a5 = touchio.TouchIn(board.A5)
 while True:
     if touch_a4.value:
         print("Touched A4!!")
+        time.sleep(0.5)
 
     if touch_a5.value:
         print("Touched A5!!")
+        time.sleep(0.5)
