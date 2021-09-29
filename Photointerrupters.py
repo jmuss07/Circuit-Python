decd287ede2c1a94f5af4bbab6384aa3114d45fa
Josephine Muss <jmuss07@ccs.local>
2021-09-29 10:22:53 -0400

    Some working photointterupter code!

---
+++ Photointerrupters.py
@@ -0,0 +1,28 @@
+# Write your code here :-)
+import board
+import time
+import digitalio
+
+interrupter_pin = digitalio.DigitalInOut(board.D8)
+interrupter_pin.switch_to_input
+interrupter_pin.pull = digitalio.Pull.UP
+
+initial_time = time.monotonic()
+photo = False
+state = False
+
+counter = 0
+max = 4
+while True:
+    photo = interrupter_pin.value
+    if photo and not state:
+        print(str(counter))
+        counter += 1
+    state = photo
+
+    new_time = max - time.monotonic()
+
+    if new_time <= 0:
+        print(str(counter))
+        max = time.monotonic() + 4
+        counter = 0
